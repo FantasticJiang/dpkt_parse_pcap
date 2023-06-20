a0bcbc12a12a181bb054f8ab6339db0df35258ee
jiangxinyu <jiangxinyu@tp-link.com.cn>
2023-06-20 11:46:09 +0800

    完成IPv4 HTTP解析处理逻辑、解析结果写入文件。
    Signed-off-by: jiangxinyu <jiangxinyu@tp-link.com.cn>

--- main.py
+++ main.py
@@ -10,15 +10,38 @@ HTTP_URL = {}
 HTTP_REFERER = {}
 HTTP_XRW = {}
 HTTP_BUNDLEID = {}
+HTTP_USER_IDENTITY = {}
 HTTP_Q_UA2 = {}
-HTTP_SPECIAL_FIELDS = [HTTP_REFERER, HTTP_XRW, HTTP_BUNDLEID, HTTP_Q_UA2]
+HTTP_X_UMENG_SDK = {}
+HTTP_SPECIAL_FIELDS = [HTTP_REFERER, HTTP_XRW, HTTP_BUNDLEID, HTTP_USER_IDENTITY, HTTP_Q_UA2, HTTP_X_UMENG_SDK]
 TLS_SNI = {}
 TLS_CN = {}
 TCP_PAYLOAD = {}
 UDP_PAYLOAD = {}
+QUIC_SNI = {}
+DNS_QUERY = {}
 other_tcp_payload_len = 30
 udp_payload_len = 30
 
+
+def write_parse_result(filename: str):
+    # HTTP
+    write_dict_to_file(HTTP_HOST, filename, '_host.log')
+    write_dict_to_file(HTTP_UA, filename, '_ua.log')
+    write_dict_to_file(HTTP_URL, filename, '_url.log')
+    if HTTP_REFERER or HTTP_XRW or HTTP_BUNDLEID or HTTP_USER_IDENTITY or HTTP_Q_UA2 or HTTP_X_UMENG_SDK:
+        write_dict_to_file(HTTP_SPECIAL_FIELDS, filename, '_special_fields.log')
+    # HTTPS
+    write_dict_to_file(TLS_SNI, filename, '_ser_name.log')
+    write_dict_to_file(TLS_CN, filename, '_cn.log')
+
+    # QUIC
+    if QUIC_SNI:
+        write_dict_to_file(QUIC_SNI, filename, '_quic_sni.log')
+
+    write_dict_to_file(TCP_PAYLOAD, filename, '_tcp_flow.log')
+    write_dict_to_file(UDP_PAYLOAD, filename, '_udp_flow.log')
+    write_dict_to_file(DNS_QUERY, filename, 'dns_query.log')
 
 
 
@@ -34,12 +57,14 @@ def parse_pcap(input_path: str):
         for ts, buf in input_pcap:
             packet_count += 1
             eth = dpkt.ethernet.Ethernet(buf)
+            # 这样写的话不支持IPv6，后续要改
             if not isinstance(eth.data, dpkt.ip.IP):
                 print("skip non IP packet.")
                 continue
             ip = eth.data
             src = socket.inet_ntoa(ip.src)
             dst = socket.inet_ntoa(ip.dst)
+            # TCP
             if isinstance(ip.data, dpkt.tcp.TCP):
                 tcp = ip.data
                 sport = tcp.sport
@@ -48,7 +73,7 @@ def parse_pcap(input_path: str):
                 src2dst_rvs = (dst, dport, src, sport)
                 # 如果本包tcp负载大于0，则根据本包所属连接是否完成握手、是否已解析过采取不同处理
                 if len(tcp.data) > 0:
-                    # 四元组已在字典中，表示已解析过。统计四元组包数量
+                    # 四元组已在字典中，表示已解析过一次。统计四元组包数量。（后续要加TLS reply方向特征解析）
                     if src2dst in tcp_4tuple_dict or src2dst_rvs in tcp_4tuple_dict:
                         try:
                             tcp_4tuple_dict[src2dst] += 1
@@ -70,15 +95,20 @@ def parse_pcap(input_path: str):
                     else:
                         tcp_handshake_comlete[src2dst] = 1
 
-
+            # UDP
             elif isinstance(ip.data, dpkt.udp.UDP):
                 parse_udp(ip.data)
             else:
                 print("skip none tcp and udp packet.")
                 continue
 
-            if packet_count == 50:
-                break
+            if packet_count%1000 == 0:
+                print(f'已解析分组数：{packet_count}')
+
+        # 解析完pcap文件中的所有分组，将特征字典写入log文件
+        import os
+        filename = os.path.splitext(input_path)[0]
+        write_parse_result(filename)
     except:
         pass
     finally:
@@ -92,7 +122,7 @@ def parse_tcp(tcp: dpkt.tcp.TCP):
     # dpkt似乎没有方法可以在读到一个packet时得知其具有的应用层协议还是仅tcp协议，解析HTTP/HTTPS目前想到的两种方式可能只能是（1）判断tcp.data是否含有HTTP/HTTPS特有的内容；（2）异常处理，先将tcp.data直接当成HTTP解析，抛出异常时当成HTTPS解析，还抛出异常则当无应用层的TCP协议解析。
     try:
         request = dpkt.http.Request(tcp.data)
-        print(f'HTTP request: {repr(request)}')
+        parse_http_request(request, tcp.dport)
     except:
         pass
 
@@ -100,7 +130,26 @@ def parse_tcp(tcp: dpkt.tcp.TCP):
 
 
 def parse_http_request(http: dpkt.http.Request, dport: int):
-    pass
+    # dpkt.http.Request对象将HTTP请求的状态行（第一行）中method、uri、version单独作为成员变量，第二行开始的所有HTTP字段放入名为header的有序字典变量中。
+    headers = http.headers
+    headers['method'] = http.method
+    headers['uri'] = http.uri
+    # basic fields
+    add_dict_kv(HTTP_HOST, headers['host']+f" [{dport}]")
+    add_dict_kv(HTTP_UA, headers['user-agent']+f" [{dport}]")
+    add_dict_kv(HTTP_URL, headers['method']+" "+headers['uri']+f" [{dport}]")
+    # special fields (if exists)
+    if 'referer' in headers:
+        add_dict_kv(HTTP_REFERER, headers['referer']+f" [{dport}]")
+    if 'bundleid' in headers:
+        add_dict_kv(HTTP_BUNDLEID, headers['bundleid']+f" [{dport}]")
+    if 'user-identity' in headers:
+        add_dict_kv(HTTP_USER_IDENTITY, headers['user-identity']+f" [{dport}]")
+    if 'x-requested-with' in headers:
+        add_dict_kv(HTTP_XRW, headers['x-requested-with']+f" [{dport}]")
+    if 'q-ua2' in headers:
+        add_dict_kv(HTTP_Q_UA2, headers['q-ua2']+f" [{dport}]")
+    # print(f'HTTP request: {repr(http)}')
 
 
 def parse_udp(udp: dpkt.udp.UDP):
@@ -109,5 +158,5 @@ def parse_udp(udp: dpkt.udp.UDP):
 
 
 if __name__ == "__main__":
-    input_pcap_file = './steam_1186http.pcap'
+    input_pcap_file = './pcaps/http_user-identity.pcap'
     parse_pcap(input_pcap_file)
--- utils.py
+++ utils.py
@@ -3,3 +3,49 @@ def add_dict_kv(d: dict, key: str):
         d[key] += 1
     except KeyError:
         d[key] = 1
+
+
+def write_dict_to_file(target_dict, filepath: str, file_suffix: str = '', ):
+    """
+    用于将字典或字典列表写入文件
+    target_dict: 待写入文件的字典
+    filepath: 文件路径
+    file_suffix: 文件名后缀（可选）
+    一般写入字典，传filepath即可，若是报文字段解析结果，可传入file_suffix表示字段。
+    示例：filepath='./test', file_suffix='_host.log'，表示将test报文的host解析结果写入'test_host.log'文件。
+    """
+    import json
+    filename = filepath + file_suffix
+    if isinstance(target_dict, dict) and target_dict:
+        f = open(filename, 'w')
+        try:
+            json.dump(target_dict, f, indent=1, ensure_ascii=False)
+        finally:
+            f.close()
+            target_dict.clear()
+    elif isinstance(target_dict, list):
+        field_list = ['Referer', 'X-Requested-With', 'bundleId', 'User-Identity', 'Q-UA2', 'X-Umeng-Sdk']
+        f = open(filename, 'w')
+        count = 0
+        for dict_num in range(len(target_dict)):
+            sub_dict = target_dict[dict_num]
+            if sub_dict:
+                if count > 0:
+                    f.write('\n')
+                f.write(field_list[dict_num]+'\n')
+                json.dump(sub_dict, f, indent=1, ensure_ascii=False)
+                f.write('\n')
+                sub_dict.clear()
+                count += 1
+        f.close()
+
+
+def write_dict_bysort(filename: str, target_dict: dict):
+    if target_dict:
+        f = open(filename, 'w')
+        for k in sorted(target_dict):
+            f.write(k + ':' + str(target_dict[k]) + '\n')
+        f.close()
+        target_dict.clear()
+
+
